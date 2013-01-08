cdaa90a3b6650af641df70347fde932f9ecab6cc
rdrury <necrosmash@gmail.com>
2013-01-08 20:10:41 +0000

    Word class created. Contains overridden equals and hascode methods. Testrunner also created.

---
+++ src/rdrury/gmit/wordcloud/TestRunner.java
@@ -0,0 +1,13 @@
+package rdrury.gmit.wordcloud;
+
+public class TestRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/rdrury/gmit/wordcloud/Word.java
@@ -0,0 +1,70 @@
+package rdrury.gmit.wordcloud;
+
+public class Word {
+	
+	private String word;
+	private int frequency;
+	
+	public Word()
+	{
+		word = "";
+		frequency = 0;
+	}
+	
+	public Word(String word)
+	{
+		this.word = word;
+	}
+	
+	public Word(String word, int frequency)
+	{
+		this.word = word;
+		this.frequency = frequency;
+	}
+	
+	public String getWord()
+	{
+		return this.word;
+	}
+	
+	public int getFrequency()
+	{
+		return this.frequency;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Word other = (Word) obj;
+		if (word == null) {
+			if (other.word != null) {
+				return false;
+			}
+		} else if (!word.equals(other.word)) {
+			return false;
+		}
+		return true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+}
